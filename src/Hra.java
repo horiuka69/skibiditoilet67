ae01b0632a254593106967441ec3307a1c492775
marin <horiuka09@gmail.com>
2026-01-19 10:34:20 +0100

    Aktualizace tridy Hra pro inicializaci HerniPlanu

--- src/Hra.java
+++ src/Hra.java
@@ -1,27 +1,35 @@
 
 public class Hra {
 
+    private HerniPlan herniPlan;
+    private boolean konecHry = false;
+
     public Hra() {
+        herniPlan = new HerniPlan();
     }
 
     public String vratUvitani() {
-        return "";
+        return "Vítejte!\n" +
+                "Toto je nová adventura.\n" +
+                "Napište 'pomoc', pokud si nevíte rady.\n\n" +
+                herniPlan.getAktualniMistnost().getDlouhyPopis();
     }
 
     public String vratEpilog() {
-        return "";
+        return "Dík, že jste si zahráli.  Ahoj.";
     }
 
     public boolean konecHry() {
-        return false;
+        return konecHry;
     }
 
     public String zpracujPrikaz(String radek) {
-        return "";
+        // TODO: Implementovat zpracování příkazů (delegace na SeznamPrikazu)
+        return "Zatím neumím zpracovat příkazy.";
     }
 
     public HerniPlan getHerniPlan() {
-        return null;
+        return herniPlan;
     }
 
     public void setKonecHry(boolean konecHry) {
